a668d0978a4604be3c78e5a76180043bbe1da22f
Explorarthecreator <fredadejiemma99@gmail.com>
2024-04-04 17:38:51 +0100

    Created the homepage and started working on the nav component

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,11 @@
+import NavBar from "../components/NavBar"
+
+function LandingPage() {
+  return (
+    <div className=" h-screen bg-slate-300">
+        <NavBar/>
+    </div>
+  )
+}
+
+export default LandingPage
